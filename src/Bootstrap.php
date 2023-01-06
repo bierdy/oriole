62477cf64ce1fe94a231cfdb51b1ae753df5a34f
bierdy <bierdy@yandex.ru>
2023-01-07 02:03:53 +0400

    Add Bootstrap class

---
+++ src/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oriole;
+
+class Bootstrap
+{
+    
+    /**
+     * Run Oriole application
+     *
+     * @return void
+     */
+    public function run() : void
+    {
+    
+    }
+}
