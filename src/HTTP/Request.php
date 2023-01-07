e4d53e0a01bfdb4397b64b210ef54c448bcad10f
bierdy <bierdy@yandex.ru>
2023-01-07 22:13:16 +0400

    Add Request class

---
+++ src/HTTP/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oriole\HTTP;
+
+/**
+ * Representation of an HTTP request.
+ */
+class Request
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    
+    }
+}
