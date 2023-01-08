bcd5f29d3b4285568d5d9a9aeccf6fa3417834bd
bierdy <bierdy@yandex.ru>
2023-01-08 17:52:34 +0400

    Include Common.php at \Oriole\Bootstrap::run Add "defineConstants" and "addRoutes" methods at \Oriole\Bootstrap and run it at \Oriole\Bootstrap::run

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,7 +4,6 @@ namespace Oriole;
 
 class Bootstrap
 {
-    
     /**
      * Run Oriole application
      *
@@ -12,6 +11,28 @@ class Bootstrap
      */
     public function run() : void
     {
+        require_once 'Common.php';
+        
+        $this->defineConstants();
+        $this->addRoutes();
+    }
     
+    protected function defineConstants() : void
+    {
+        define('ORIOLE_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+        define('ORIOLE_CONFIG_PATH', ORIOLE_PATH . 'Config' . DIRECTORY_SEPARATOR);
+    }
+    
+    /**
+     * Add routes
+     *
+     * @return void
+     */
+    protected function addRoutes(): void
+    {
+        if (defined('CONFIG_PATH') && is_file(CONFIG_PATH . 'Routes.php'))
+            require_once CONFIG_PATH . 'Routes.php';
+        
+        require_once ORIOLE_CONFIG_PATH . 'Routes.php';
     }
 }
