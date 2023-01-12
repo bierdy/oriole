58c6f331b2420397dc2bbf6af5fcc2877de028d9
bierdy <bierdy@yandex.ru>
2023-01-12 10:27:14 +0400

    Add \Oriole\Config\Database class

---
+++ src/Config/Database.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Oriole\Config;
+
+class Database extends BaseConfig
+{
+    protected string $hostname = '';
+    protected string $username = '';
+    protected string $password = '';
+    protected string $database = '';
+    protected string $prefix = 'oriole_';
+    protected string $charset = 'utf8mb4';
+    protected string $collat = 'utf8mb4_unicode_ci';
+    protected int $port = 3306;
+}
