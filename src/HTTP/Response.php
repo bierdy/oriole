34fce4e1546939729f68eab214fc81515eebb5ed
bierdy <bierdy@yandex.ru>
2023-01-12 19:25:48 +0400

    Let's make \Oriole\HTTP\Response singleton class

--- src/HTTP/Response.php
+++ src/HTTP/Response.php
@@ -2,16 +2,41 @@
 
 namespace Oriole\HTTP;
 
+use Exception;
+
 /**
- * Representation of an HTTP request.
+ * Representation of an HTTP response.
  */
 class Response
 {
+    protected static self|null $instance = null;
+    
     /**
      * Constructor
      */
-    public function __construct()
+    final private function __construct()
     {
     
+    }
+    final protected function __clone()
+    {
+        
+    }
+    
+    /**
+     * @throws Exception
+     */
+    final public function __wakeup()
+    {
+        throw new Exception('Cannot unserialize a singleton.');
+    }
+    
+    public static function getInstance() : self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self;
+        }
+        
+        return self::$instance;
     }
 }
