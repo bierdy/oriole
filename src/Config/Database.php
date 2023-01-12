82e35fa5ec5eb4bb19e99a43b328f7061e1e1606
bierdy <bierdy@yandex.ru>
2023-01-12 16:32:40 +0400

    Delete \Oriole\Config\Database::prefix property

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -8,7 +8,6 @@ class Database extends BaseConfig
     protected string $username = '';
     protected string $password = '';
     protected string $database = '';
-    protected string $prefix = 'oriole_';
     protected string $charset = 'utf8mb4';
     protected string $collation = 'utf8mb4_unicode_ci';
     protected int $port = 3306;
