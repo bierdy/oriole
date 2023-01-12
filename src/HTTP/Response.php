55c19698a5da21d4fe067d13518b8598f68a27b1
bierdy <bierdy@yandex.ru>
2023-01-12 19:00:24 +0400

    Add \Oriole\HTTP\Response class

---
+++ src/HTTP/Response.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oriole\HTTP;
+
+/**
+ * Representation of an HTTP request.
+ */
+class Response
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    
+    }
+}
