8d2d95d5215cf21451b4eba04f1eef121e7aa124
bierdy <bierdy@yandex.ru>
2023-01-16 10:59:15 +0400

    Set default value of \Oriole\Config\Cookie::expires as 0

--- src/Config/Cookie.php
+++ src/Config/Cookie.php
@@ -5,7 +5,7 @@ namespace Oriole\Config;
 class Cookie extends BaseConfig
 {
     protected string $prefix = 'oriole_';
-    protected int $expires = 365;
+    protected int $expires = 0;
     protected string $path = '/';
     protected string $domain = '';
     protected bool $secure = false;
