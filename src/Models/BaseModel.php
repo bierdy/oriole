265b729bb76486181b876ccbabab6c6b783e8b75
bierdy <bierdy@yandex.ru>
2023-01-20 19:43:07 +0400

    Add base methods at \Oriole\Models\BaseModel class

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -7,7 +7,28 @@ use Oriole\Oriole;
 class BaseModel
 {
     protected static \PDO|null $dbh = null;
+    
     protected \PDOStatement|false $sth = false;
+    
+    public string $table = '';
+    
+    public string $primaryKey = '';
+    
+    public array $validationRules = [];
+    
+    public array $validationMessages = [];
+    
+    protected ? int $fetchMode = null;
+    
+    protected string $sql = '';
+    
+    final const BIND_KEY = ':key_';
+    
+    protected int $bindCounter = 0;
+    
+    protected array $bindValues = [];
+    
+    protected array $errors = [];
     
     public function __construct()
     {
@@ -24,6 +45,7 @@ class BaseModel
             $dsn = "mysql:host={$hostname};port={$port};dbname={$database};charset={$charset}";
             $options = [
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
                 \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES '{$charset}' COLLATE '{$collation}'"
             ];
             
@@ -35,17 +57,165 @@ class BaseModel
         }
     }
     
-    public function execute(string $statement, $data = null, $fetch_mode = \PDO::FETCH_ASSOC): bool|\PDOStatement
+    public function from(string $table) : static
     {
-        $this->sth = self::$dbh->prepare($statement);
+        $this->sql .= " FROM $table ";
         
-        if (! is_null($data))
-            $this->sth->execute($data);
-        else
+        return $this;
+    }
+    
+    public function select(string $select) : static
+    {
+        $this->sql .= " SELECT $select ";
+        
+        return $this;
+    }
+    
+    public function where(string $name, string $operator, string|int|float $value, string $logic = '') : static
+    {
+        $bindName = self::BIND_KEY . $this->bindCounter;
+        $this->sql .= " $logic WHERE $name $operator $bindName ";
+        $this->bindValues[$bindName] = $value;
+        
+        $this->bindCounter++;
+        
+        return $this;
+    }
+    
+    public function andWhere(string $name, string $operator, string|int|float $value) : static
+    {
+        return $this->where($name, $operator, $value, 'AND');
+    }
+    
+    public function orWhere(string $name, string $operator, string|int|float $value) : static
+    {
+        return $this->where($name, $operator, $value, 'OR');
+    }
+    
+    public function whereIn(string $name, array $values, string $logic = '') : static
+    {
+        $whereIn = [];
+        foreach ($values as $value) {
+            $bindName = self::BIND_KEY . $this->bindCounter;
+            $whereIn[] = $bindName;
+            $this->bindValues[$bindName] = $value;
+            
+            $this->bindCounter++;
+        }
+        
+        $this->sql .= " $logic WHERE $name IN (" . implode(',', $whereIn) . ") ";
+        
+        return $this;
+    }
+    
+    public function andWhereIn(string $name, array $values) : static
+    {
+        return $this->whereIn($name, $values, 'AND');
+    }
+    
+    public function orWhereIn(string $name, array $values) : static
+    {
+        return $this->whereIn($name, $values, 'OR');
+    }
+    
+    public function join(string $type, string $table, string $on) : static
+    {
+        $type = strtoupper($type);
+        $this->sql .= " $type JOIN $table ON $on ";
+        
+        return $this;
+    }
+    
+    public function groupBy(string $name) : static
+    {
+        $this->sql .= " GROUP BY $name ";
+        
+        return $this;
+    }
+    
+    public function orderBy(string $name) : static
+    {
+        $this->sql .= " ORDER BY $name ";
+        
+        return $this;
+    }
+    
+    public function setFetchMode(int $fetchMode) : static
+    {
+        $this->fetchMode = $fetchMode;
+        
+        return $this;
+    }
+    
+    public function execute() : void
+    {
+        $this->errors = [];
+        
+        try {
+            $this->sth = self::$dbh->prepare($this->sql);
+            
+            foreach ($this->bindValues as $bindKey => $bindValue)
+                $this->sth->bindValue(substr($bindKey, 1), $bindValue);
+            
+            if (! is_null($this->fetchMode))
+                $this->sth->setFetchMode($this->fetchMode);
+            
             $this->sth->execute();
+        } catch (\PDOException $e) {
+            $this->errors[] = $e->getMessage();
+        }
+    }
+    
+    public function findAll() : false|array
+    {
+        $this->execute();
         
-        $this->sth->setFetchMode($fetch_mode);
+        return empty($this->errors) ? $this->sth->fetchAll() : false;
+    }
+    
+    public function findOne()
+    {
+        $this->execute();
         
-        return $this->sth;
+        return empty($this->errors) ? $this->sth->fetch() : false;
+    }
+    
+    public function getAll(array $values = []) : false|array
+    {
+        if (empty($this->table))
+            $this->errors[] = 'Table key is empty';
+        
+        if (empty($this->primaryKey))
+            $this->errors[] = 'Primary key is empty';
+        
+        if (! empty($this->errors))
+            return false;
+        
+        $this->select('*')->from($this->table);
+        
+        if (! empty($values))
+            $this->whereIn($this->primaryKey, $values);
+        
+        $this->execute();
+        
+        return empty($this->errors) ? $this->sth->fetchAll() : false;
+    }
+    
+    public function getOne(string|int|float $value)
+    {
+        if (empty($this->table))
+            $this->errors[] = 'Table key is empty';
+        
+        if (empty($this->primaryKey))
+            $this->errors[] = 'Primary key is empty';
+        
+        if (! empty($this->errors))
+            return false;
+        
+        $this->select('*')->from($this->table)->where($this->primaryKey, '=', $value);
+        
+        $this->execute();
+        
+        return empty($this->errors) ? $this->sth->fetch() : false;
     }
 }
