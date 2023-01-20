c503da3c577cea9013012a35f50a1ffbdc9718d3
bierdy <bierdy@yandex.ru>
2023-01-20 19:46:06 +0400

    Add main properties at \Oriole\Models\LanguageModel class

--- src/Models/LanguageModel.php
+++ src/Models/LanguageModel.php
@@ -4,5 +4,16 @@ namespace Oriole\Models;
 
 class LanguageModel extends BaseModel
 {
-
+    public string $table = 'oriole_languages';
+    
+    public string $primaryKey = 'id';
+    
+    public array $validationRules = [
+        'title' => 'required|is_unique[languages.title,id,{id}]',
+        'code' => 'is_unique[languages.code,id,{id}]',
+        'icon' => 'required',
+        'order' => 'required|numeric',
+    ];
+    
+    public array $validationMessages = [];
 }
