4acc38c49f57f2454e9e8111635877f0c9ed5b43
bierdy <bierdy@yandex.ru>
2023-02-02 13:00:00 +0400

    Add methods addOne, validate, fillPlaceholders, processRules, isStringList, getErrorMessage, errors at \Oriole\Models\BaseModel class

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -43,7 +43,7 @@ class BaseModel
     
     protected string $sql = '';
     
-    final const BIND_KEY = ':key_';
+    final protected const BIND_KEY = ':key_';
     
     protected int $bindCounter = 0;
     
@@ -286,5 +286,271 @@ class BaseModel
         $this->execute();
         
         return empty($this->errors) ? $this->stmt->fetch() : false;
+    }
+    
+    /**
+     * @throws Exception
+     */
+    public function addOne(array|object $values) : int|string|false
+    {
+        $values = is_object($values) ? (array) $values : $values;
+        $this->validate($values, false);
+        
+        if (! empty($this->errors))
+            return false;
+        
+        unset($values[$this->primaryKey], $values[$this->createdAtKey], $values[$this->updatedAtKey]);
+        
+        $names = [];
+        $keys = [];
+        foreach ($values as $name => $value) {
+            $names[] = $name;
+            $key = self::BIND_KEY . $this->bindCounter;
+            $keys[] = $key;
+            $this->binds[$key] = $value;
+            
+            $this->bindCounter++;
+        }
+        
+        $this->sql .= " INSERT INTO {$this->table} (" . implode(',', $names) . ") VALUES (" . implode(',', $keys) . ") ";
+        
+        $this->execute();
+        
+        return empty($this->errors) ? self::$dbh->lastInsertId() : false;
+    }
+    
+    /**
+     * @throws Exception
+     */
+    protected function validate(array $data, bool $onlyPassedData = true) : void
+    {
+        if (empty($data)) {
+            $this->errors['logic'][] = 'There is no data to validate';
+            return;
+        }
+        
+        if (empty($validationRules = $this->validationRules))
+            return;
+        
+        $validationRules = $onlyPassedData ? array_intersect_key($validationRules, $data) : $validationRules;
+        
+        // Replace any placeholders (e.g. {id}) in the rules with
+        // the value found in $data, if any.
+        $validationRules = $this->fillPlaceholders($validationRules, $data);
+        
+        foreach ($validationRules as $field => $rules) {
+            $rules = explode('|', $rules);
+            
+            if (str_contains($field, '*')) {
+                $values = array_filter(array_flatten_with_dots($data), static fn ($key) => preg_match(
+                    '/^'
+                    . str_replace(['\.\*', '\*\.'], ['\..+', '.+\.'], preg_quote($field, '/'))
+                    . '$/',
+                    $key
+                ), ARRAY_FILTER_USE_KEY);
+                // if keys not found
+                $values = $values ? : [$field => null];
+            } else {
+                $values = dot_array_search($field, $data);
+            }
+            
+            if ($values === []) {
+                // We'll process the values right away if an empty array
+                $this->processRules($field, $values, $rules, $data);
+                
+                continue;
+            }
+            
+            if (str_contains($field, '*')) {
+                // Process multiple fields
+                foreach ($values as $dotField => $value)
+                    $this->processRules($dotField, $value, $rules, $data, $field);
+            } else {
+                // Process single field
+                $this->processRules($field, $values, $rules, $data);
+            }
+        }
+    }
+    
+    /**
+     * Replace any placeholders within the rules with the values that
+     * match the 'key' of any properties being set. For example, if
+     * we had the following $data array:
+     *
+     * [ 'id' => 13 ]
+     *
+     * and the following rule:
+     *
+     *  'required|is_unique[users,email,id,{id}]'
+     *
+     * The value of {id} would be replaced with the actual id in the form data:
+     *
+     *  'required|is_unique[users,email,id,13]'
+     */
+    protected function fillPlaceholders(array $rules, array $data) : array
+    {
+        if (empty($rules) || empty($data))
+            return $rules;
+        
+        $replacements = [];
+        
+        foreach ($data as $key => $value)
+            $replacements["{{$key}"] = $value;
+        
+        foreach ($rules as &$rule)
+            $rule = strtr($rule, $replacements);
+        
+        return $rules;
+    }
+    
+    /**
+     * Runs all of $rules against $field, until one fails, or
+     * all of them have been processed. If one fails, it adds
+     * the error to $this->errors and moves on to the next,
+     * so that we can collect all the first errors.
+     *
+     * @param string $field
+     * @param array|string $value
+     * @param array $rules
+     * @param array|null $data The array of data to validate.
+     * @param string|null $originalField The original asterisk field name like "foo.*.bar".
+     * @return bool
+     * @throws Exception
+     */
+    protected function processRules(string $field, array|string $value, array $rules, ? array $data = null, ? string $originalField = null) : bool
+    {
+        if (is_null($data))
+            throw new InvalidArgumentException('You must supply the parameter: data.');
+        
+        if (in_array('if_exist', $rules, true)) {
+            $flattenedData = array_flatten_with_dots($data);
+            $ifExistField  = $field;
+            
+            if (str_contains($field, '.*')) {
+                // We'll change the dot notation into a PCRE pattern that can be used later
+                $ifExistField = str_replace('\.\*', '\.(?:[^\.]+)', preg_quote($field, '/'));
+                $dataIsExisting = false;
+                $pattern = sprintf('/%s/u', $ifExistField);
+                
+                foreach (array_keys($flattenedData) as $item) {
+                    if (preg_match($pattern, $item) === 1) {
+                        $dataIsExisting = true;
+                        break;
+                    }
+                }
+            } else {
+                $dataIsExisting = array_key_exists($ifExistField, $flattenedData);
+            }
+            
+            unset($ifExistField, $flattenedData);
+            
+            if (! $dataIsExisting) {
+                // we return early if 'if_exist' is not satisfied. we have nothing to do here.
+                return true;
+            }
+            
+            // Otherwise remove the if_exist rule and continue the process
+            $rules = array_diff($rules, ['if_exist']);
+        }
+        
+        if (in_array('permit_empty', $rules, true)) {
+            if (! in_array('required', $rules, true) && (is_array($value) ? $value === [] : trim($value) === ''))
+                return true;
+            
+            $rules = array_diff($rules, ['permit_empty']);
+        }
+        
+        foreach ($rules as $rule) {
+            $isRuleMethodExist = method_exists($this->validationRulesHandler, $rule);
+            $param  = false;
+            
+            if (! $isRuleMethodExist && preg_match('/(.*?)\[(.*)/', $rule, $match)) {
+                $rule  = $match[1];
+                $param = $match[2];
+            }
+    
+            $isRuleMethodExist = method_exists($this->validationRulesHandler, $rule);
+            
+            // If it's a callable, call and get out of here.
+            if ($isRuleMethodExist)
+                $passed = $param === false ? $this->validationRulesHandler->$rule($value) : $this->validationRulesHandler->$rule($value, $param, $data);
+            else
+                throw new Exception("'$rule' is not a valid rule.");
+            
+            // Set the error message if we didn't survive.
+            if ($passed === false) {
+                // if the $value is an array, convert it to as string representation
+                if (is_array($value))
+                    $value = $this->isStringList($value) ? '[' . implode(', ', $value) . ']' : json_encode($value);
+                elseif (is_object($value))
+                    $value = json_encode($value);
+                
+                $param = ($param === false) ? '' : $param;
+                
+                $this->errors['data'][$field] = $this->getErrorMessage(
+                    $rule,
+                    $field,
+                    $param,
+                    (string) $value,
+                    $originalField
+                );
+                
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Is the array a string list 'list<string>'?
+     */
+    private function isStringList(array $array) : bool
+    {
+        $expectedKey = 0;
+        
+        foreach ($array as $key => $val) {
+            // Note: also covers PHP array key conversion, e.g. '5' and 5.1 both become 5
+            if (! is_int($key))
+                return false;
+            
+            if ($key !== $expectedKey)
+                return false;
+            
+            $expectedKey++;
+            
+            if (! is_string($val))
+                return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Attempts to find the appropriate error message
+     *
+     * @param string|null $value The value that caused the validation to fail.
+     */
+    protected function getErrorMessage(string $rule, string $field, ? string $param = null, ? string $value = null, ? string $originalField = null) : string
+    {
+        if (isset($this->validationMessages[$field][$rule]))
+            $message = $this->validationMessages[$field][$rule];
+        elseif (! is_null($originalField) && isset($this->validationMessages[$originalField][$rule]))
+            $message = $this->validationMessages[$originalField][$rule];
+        elseif (isset($this->validationMessagesHandler->messages[$rule]))
+            $message = $this->validationMessagesHandler->messages[$rule];
+        else
+            $message = "There is an error with field {field}";
+        
+        $message = str_replace('{field}', $field, $message);
+        $message = str_replace('{param}', $param ?? '', $message);
+        $message = str_replace('{value}', $value ?? '', $message);
+        
+        return $message;
+    }
+    
+    public function errors() : array
+    {
+        return $this->errors;
     }
 }
