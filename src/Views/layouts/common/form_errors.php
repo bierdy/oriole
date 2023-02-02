2db6fea0d0b094496e071d060f038e7659fe8953
bierdy <bierdy@yandex.ru>
2023-02-02 15:50:13 +0400

    Modify form errors view

--- src/Views/layouts/common/form_errors.php
+++ src/Views/layouts/common/form_errors.php
@@ -1,8 +1,20 @@
 <div class="alert alert-danger">
     <?= $message; ?>
-    <?php foreach ($errors as $errorGroup) : ?>
-        <?php foreach ($errorGroup as $error) : ?>
+    <?php if (! empty($errors['pdo'])) { ?>
+        <?php foreach ($errors['pdo'] as $error) : ?>
             <div><?= $error; ?></div>
         <?php endforeach ?>
-    <?php endforeach ?>
+    <?php } ?>
+    <?php if (! empty($errors['logic'])) { ?>
+        <?php foreach ($errors['logic'] as $error) : ?>
+            <div><?= $error; ?></div>
+        <?php endforeach ?>
+    <?php } ?>
+    <?php if (! empty($errors['data'])) { ?>
+        <?php foreach ($errors['data'] as $data) : ?>
+            <?php foreach ($data as $error) : ?>
+                <div><?= $error; ?></div>
+            <?php endforeach ?>
+        <?php endforeach ?>
+    <?php } ?>
 </div>
