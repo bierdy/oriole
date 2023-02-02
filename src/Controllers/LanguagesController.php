cce8ba2626e13730c8451d6a82ebae1e6b30c6e2
bierdy <bierdy@yandex.ru>
2023-02-02 13:01:18 +0400

    Add \Oriole\Controllers\LanguagesController::list() method

--- src/Controllers/LanguagesController.php
+++ src/Controllers/LanguagesController.php
@@ -4,5 +4,25 @@ namespace Oriole\Controllers;
 
 class LanguagesController extends BaseController
 {
-
+    public function list()
+    {
+        $languages = $this->baseModel
+            ->select("l.*, COUNT(DISTINCT v.id) AS variables_count")
+            ->from("{$this->languageModel->table} AS l")
+            ->join('left', "{$this->variableModel->table} AS v", 'l.id = v.language_id')
+            ->groupBy('l.id')
+            ->orderBy('l.id')
+            ->findAll();
+        
+        $variables_count = array_reduce($languages, fn($count, $language) => $count + $language->variables_count, 0);
+        
+        $custom_data = [
+            'title' => 'Languages',
+            'languages' => $languages,
+            'variables_count' => $variables_count,
+        ];
+        $data = array_merge($this->default_data, $custom_data);
+        
+        return $this->baseView->render('templates/languages/list.php', $data);
+    }
 }
