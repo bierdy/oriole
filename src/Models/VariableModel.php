433246f36753bbf25bcbd29f086fbc53fa72dedd
bierdy <bierdy@yandex.ru>
2023-02-02 22:28:49 +0400

    Fix \Oriole\Models\VariableModel::validationRules property

--- src/Models/VariableModel.php
+++ src/Models/VariableModel.php
@@ -9,8 +9,8 @@ class VariableModel extends BaseModel
     public string $primaryKey = 'id';
     
     public array $validationRules = [
-        'title' => 'required|is_unique[variables.title,id,{id}]',
-        'name' => 'required|is_unique[variables.name,id,{id}]|alpha_dash',
+        'title' => 'required|is_unique[oriole_variables.title,id,{id}]',
+        'name' => 'required|is_unique[oriole_variables.name,id,{id}]|alpha_dash',
         'class' => 'required|is_class_exist|class_is_not_implement_interface[Wagtail\Variables\VariableInterface]',
         'options' => 'permit_empty|valid_json',
         'template' => 'required',
