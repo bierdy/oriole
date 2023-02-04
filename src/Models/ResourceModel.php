afa1f719f09de3ff21c2a3eee6e6a17be0c5e59b
bierdy <bierdy@yandex.ru>
2023-02-04 11:24:45 +0400

    Add \Oriole\Models\ResourceModel class

---
+++ src/Models/ResourceModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Oriole\Models;
+
+class ResourceModel extends BaseModel
+{
+    public string $table = 'oriole_resources';
+    
+    public string $primaryKey = 'id';
+    
+    public array $validationRules = [
+        'title' => 'required|is_unique[oriole_resources.title,id,{id}]',
+        'parent_id' => 'required|numeric',
+        'template_id' => 'required|numeric',
+        'sort_order' => 'required|numeric',
+    ];
+    
+    public array $validationMessages = [];
+}
