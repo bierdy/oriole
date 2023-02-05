3e9b1cc1e77bd5fb82d7ba27d5b5aca7b3436743
bierdy <bierdy@yandex.ru>
2023-02-05 21:03:28 +0400

    Add \Oriole\Models\TemplateModel::getAvailableTemplates() method

--- src/Models/TemplateModel.php
+++ src/Models/TemplateModel.php
@@ -14,4 +14,24 @@ class TemplateModel extends BaseModel
     ];
     
     public array $validationMessages = [];
+    
+    /*
+     * Get non-unique templates plus unique templates that are not assigned on any resource.
+     */
+    public function getAvailableTemplates() : array
+    {
+        $resourceModel = new ResourceModel();
+        
+        return $this
+            ->select("{$this->table}.*")
+            ->from($this->table)
+            ->whereNotIn('id', function ($templateModel) use ($resourceModel) {
+                $templateModel
+                    ->select('r.template_id')
+                    ->from($resourceModel->table . ' AS r')
+                    ->join('left', "{$this->table} AS t", 't.id = r.template_id')
+                    ->where('t.is_unique', '=', 1);
+            })
+            ->findAll();
+    }
 }
